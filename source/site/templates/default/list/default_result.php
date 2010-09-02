170cfec321a08ca00f1a063e7f43c23ecf7a1609
Gaurav <gaurav@readybytes.in>
2010-09-02 05:34:29 +0000

    Moved template files

---
+++ source/site/templates/default/list/default_result.php
@@ -0,0 +1,7 @@
+<?php
+/**
+* @Copyright Ready Bytes Software Labs Pvt. Ltd. (C) 2010- author-Team Joomlaxi
+* @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
+**/
+
+include_once(dirname(__FILE__).DS.'..'.DS.'result.php');
