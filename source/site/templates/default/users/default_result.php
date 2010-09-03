38cdb12f56bdac2a7aeb68e4873bfa687baaf815
Gaurav <gaurav@readybytes.in>
2010-09-03 06:04:25 +0000

    File removed of Template // default // users // default_result.php

--- source/site/templates/default/users/default_result.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
-* @Copyright Ready Bytes Software Labs Pvt. Ltd. (C) 2010- author-Team Joomlaxi
-* @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
-**/
-
-include_once(dirname(dirname(__FILE__)).DS.'result.php');
