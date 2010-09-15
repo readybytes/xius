9ed0ae4a5502ce9bcefbefedf98dab4b9507560a
Gaurav <gaurav@readybytes.in>
2010-09-15 06:17:24 +0000

    bug of List CSS is fixed if default template

--- source/site/templates/default/list/default_lists.php
+++ source/site/templates/default/list/default_lists.php
@@ -3,7 +3,7 @@
 * @Copyright Ready Bytes Software Labs Pvt. Ltd. (C) 2010- author-Team Joomlaxi
 * @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
 **/
-$this->loadAssets('css', 'list.css');
+$this->loadAssets('css', 'lists.css');
 
 ?>
 <div class="">
