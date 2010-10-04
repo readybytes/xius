b6f87e612af9e8fd2639ffff39d451bb318f472f
Manish <manish@readybytes.in>
2010-10-04 08:39:00 +0000

    # Now our plugin work with JS integration

--- source/admin/libraries/controller.php
+++ source/admin/libraries/controller.php
@@ -18,5 +18,27 @@ abstract class XiusController extends XiusAdminController
 	{
 		parent::__construct();
 	}
+	
+	function execute( $task )
+	{
+		$plugin	= JRequest::getCmd('plugin',	'');
+		if($plugin != ''){
+			$pInst = XiusFactory::getPluginInstance($plugin);
+			
+			if($pInst == false)
+				JError::raiseError( 500 , sprintf(XiusText::_('Invalid XIUS Plugin Object %s. Class definition does not exists in this context.' )));
+
+			$controllerClass = 'Xius'.'PluginController'.JString::ucfirst(JString::strtolower($plugin));
+			$controller = $pInst->getController($controllerClass);
+			if($controller == false)
+				JError::raiseError( 500 , sprintf(XiusText::_('Invalid Plugin Controller Object %s. Class definition does not exists in this context.' ),$controller));
+
+			$controller->execute($task);
+			$controller->redirect();
+			return;
+		}
+		
+		parent::execute($task);
+	}
 
 }
--- source/site/xius.php
+++ source/site/xius.php
@@ -11,28 +11,9 @@ require_once  dirname(__FILE__).DS.'includes.php';
 $view	= JRequest::getCmd('view', 		'users');
 $task 	= JRequest::getCmd('task', 		'panel');
 $format	= JRequest::getCmd('format',	'html');
-$plugin	= JRequest::getCmd('plugin',	'');
 
-/*IMP : discard this assumption b'coz view name always set after View
-*we assume that controller name will be before , 'controller' in front
-*/
-if($plugin != ''){
-	$pInst = XiusFactory::getPluginInstance($plugin);
-	if($pInst == false)
-		JError::raiseError( 500 , sprintf(XiusText::_('Invalid XIUS Plugin Object %s. Class definition does not exists in this context.' )));
-
-	$controllerClass = 'Xius'.'PluginController'.JString::ucfirst(JString::strtolower($plugin));
-	$controller = $pInst->getController($controllerClass);
-	if($controller == false)
-		JError::raiseError( 500 , sprintf(XiusText::_('Invalid Plugin Controller Object %s. Class definition does not exists in this context.' ),$controller));
-
-	$controller->execute($task);
-	$controller->redirect();
-	return;
-}
-
-$controllerClass = 'Xiussite'.'Controller'.JString::ucfirst(JString::strtolower($view));
-
+$controllerClass = 'Xiussite'.'Controller'
+				  .JString::ucfirst(JString::strtolower($view));
 
 // Test if the object really exists in the current context
 if(class_exists( $controllerClass,true)===false)
