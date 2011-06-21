a9978ce241fbebb662f7dacf693d15375dd1f5d7
Anish <anish@readybytes.in>
2011-06-21 11:33:16 +0000

    Change in xius.xml for copy form folder. Change in sql file according to joomla versions.

--- test/test/unit/com/admin/controllers/sql/XiusControllerListTest/16/testSaveList.start.sql
+++ test/test/unit/com/admin/controllers/sql/XiusControllerListTest/16/testSaveList.start.sql
@@ -10,3 +10,16 @@ INSERT INTO `#__xius_info` (`id`, `labelName`, `params`, `key`, `pluginParams`, 
 (6, 'Name', 'isSearchable=1\nisVisible=1\nisSortable=1\nisExportable=1', 'name', '', 'Joomla', 6, 1),
 (7, 'Checkbox1', 'isSearchable=1\nisVisible=0\nisSortable=0\nisExportable=1', '17', '', 'Jsfields', 7, 1),
 (8, 'Birthday', 'isSearchable=1\nisVisible=1\nisSortable=1\nisExportable=1', '3', '', 'Jsfields', 8, 1);;
+
+TRUNCATE TABLE `#__xius_list`;;
+ALTER TABLE `#__xius_list` AUTO_INCREMENT=1;;
+
+DROP TABLE IF EXISTS `au_#__xius_list`;;
+CREATE TABLE `au_#__xius_list` SELECT * FROM `#__xius_list`;;
+INSERT INTO `au_#__xius_list` (`id`, `owner`, `name`, `visibleinfo`, `sortinfo`, `sortdir`, `join`, `conditions`, `published`, `ordering`, `description`, `params`) VALUES
+(1, 62, 'Male from Afghanistan', '', '3', 'ASC', 'OR', 'a:2:{i:0;a:3:{s:6:"infoid";s:1:"1";s:5:"value";s:4:"Male";s:8:"operator";s:1:"=";}i:1;a:3:{s:6:"infoid";s:1:"3";s:5:"value";s:11:"Afghanistan";s:8:"operator";s:1:"=";}}', 0, 2, '<b>Male from Afghanistan</b>','js_privacy="public"\nxiusListViewGroup="a:1:{i:0;s:3:"All";}"'),
+(2, 62, 'Register Date', '', 2, 'DESC', 'OR', 'a:1:{i:0;a:3:{s:6:"infoid";s:1:"4";s:5:"value";s:10:"16-01-2010";s:8:"operator";s:1:"=";}}', 1, 2, '<b>Register</b>', 'js_privacy="public"\nxiusListViewGroup="a:1:{i:0;s:3:"All";}"');;
+
+INSERT INTO `#__xius_list` (`id`, `owner`, `name`, `visibleinfo`, `sortinfo`, `sortdir`, `join`, `conditions`, `published`, `ordering`, `description`, `params`) VALUES
+(1, 62, 'Male from Afghanistan', '', '4', 'DESC', 'AND', 'a:2:{i:0;a:3:{s:6:"infoid";s:1:"1";s:5:"value";s:4:"Male";s:8:"operator";s:1:"=";}i:1;a:3:{s:6:"infoid";s:1:"3";s:5:"value";s:11:"Afghanistan";s:8:"operator";s:1:"=";}}', 1, 2, 'All Male from Afghanistan', ''),
+(2, 62, 'Register Date is 16-01-2010', '', '3', 'ASC', 'AND', 'a:1:{i:0;a:3:{s:6:"infoid";s:1:"4";s:5:"value";s:10:"16-01-2010";s:8:"operator";s:1:"=";}}', 1, 2, 'All members whose registeration date is 16 Jan 2010', '');;
